7679e194133dc607d111d9fc565854923244db7a
stealthness <stevewestinlondon@gmail.com>
2022-08-01 13:02:47 +0100

    print empty board done

--- problem-001-tic-tac-toe/main.py
+++ problem-001-tic-tac-toe/main.py
@@ -9,8 +9,23 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_board(board):
+    for i in range(3):
+        print(f"{board[3*i+0]} | {board[3*i+0]} | {board[3*i+0]}")
+        if i > 1:
+            return
+        print(f'- - - - -')
+
+
+def play_game():
+    print('Start game\n')
+    board = ['.']*9
+    print_board(board)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    play_game()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
