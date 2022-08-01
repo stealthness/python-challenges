37a23af0f4ea7564486bae5cb0e1d88363f49746
stealthness <stevewestinlondon@gmail.com>
2022-08-01 13:15:20 +0100

    working on code

--- problem-001-tic-tac-toe/main.py
+++ problem-001-tic-tac-toe/main.py
@@ -11,16 +11,42 @@ def print_hi(name):
 
 def print_board(board):
     for i in range(3):
-        print(f"{board[3*i+0]} | {board[3*i+0]} | {board[3*i+0]}")
+        print(f"{board[3*i+0]} | {board[3*i+1]} | {board[3*i+2]}")
         if i > 1:
             return
         print(f'- - - - -')
 
 
+def print_position_selection():
+    for i in range(3):
+        print(f"{3 * i + 1} | {3 * i + 2} | {3 * i + 3}")
+        if i > 1:
+            return
+        print(f'- - - - -')
+
+
+def if_valid_move(move_selection):
+    # not a complete function
+    return move_selection == '1'
+
+
+def make_move(player_no: int):
+    valid_move = False
+    while not valid_move:
+        move_selection = input(f'player{player_no} select your move')
+
+        if if_valid_move(move_selection):
+            return
+        else:
+            print('not a valid move')
+
 def play_game():
     print('Start game\n')
     board = ['.']*9
     print_board(board)
+    print('Select position using:')
+    print_position_selection()
+    make_move(1)
 
 
 # Press the green button in the gutter to run the script.
