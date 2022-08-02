448ade378ed59873e36baaa06283ffb91a3b268f
stealthness <stevewestinlondon@gmail.com>
2022-08-02 11:54:12 +0100

    added loop for making moves added check for no more moves added check for at least horizontal victory conditions

--- problem-001-tic-tac-toe/main.py
+++ problem-001-tic-tac-toe/main.py
@@ -25,20 +25,42 @@ def print_position_selection():
         print(f'- - - - -')
 
 
-def if_valid_move(move_selection):
-    # not a complete function
-    return move_selection == '1'
+def if_valid_move(board, move_selection, player_mark):
+    if board[move_selection - 1] == '.':
+        board[move_selection - 1] = player_mark
+        return True
+
+    return False
 
 
-def make_move(player_no: int):
+def make_move(board, player_mark: str):
     valid_move = False
     while not valid_move:
-        move_selection = input(f'player{player_no} select your move')
+        move_selection = int(input(f'player{player_mark} select your move'))
 
-        if if_valid_move(move_selection):
+        if if_valid_move(board, move_selection, player_mark):
             return
         else:
-            print('not a valid move')
+            print('not a valid move, try again')
+
+
+def is_game_over(board):
+    """
+    Returns True if all the spaces are filled. Does not determine if the game ended by victory
+    :param board:
+    :return:
+    """
+    for s in board:
+        if s == '.':
+            return False
+    return True
+
+
+def has_player_won(board, player_mark):
+    for i in range(3):
+        if [board[i*3], board[i*3+1], board[i*3+2]] == ['O', 'O', 'O']:
+            return True
+
 
 def play_game():
     print('Start game\n')
@@ -46,7 +68,20 @@ def play_game():
     print_board(board)
     print('Select position using:')
     print_position_selection()
-    make_move(1)
+
+    while not is_game_over(board):
+        make_move(board, 'O')
+        print_board(board)
+        if has_player_won(board, 'O'):
+            print(f'PlayerO has won')
+            return
+        if is_game_over(board):
+            return
+        make_move(board, 'X')
+        print_board(board)
+        if has_player_won(board, 'X'):
+            print(f'PlayerX has won')
+            return
 
 
 # Press the green button in the gutter to run the script.
