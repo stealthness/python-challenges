f3f37ca9c2de7377b037c577adc60aeb1fddde10
stealthness <stevewestinlondon@gmail.com>
2022-08-02 14:04:05 +0100

    small progress so adding some random stars to background

--- problem-002-starfield/main.py
+++ problem-002-starfield/main.py
@@ -1,8 +1,11 @@
+import random
 from tkinter import *
 
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 400
 WINDOW_POS = '300+300'
+STAR_SIZE = 2
+INITIAL_NUMBER_OF_STARS = 10
 
 
 def print_hi(name):
@@ -10,11 +13,27 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def run():
-    ws = Tk()
+def create_random_starfield():
+    stars = []
+    for i in range(INITIAL_NUMBER_OF_STARS):
+        stars.append([random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)])
+    return stars
 
-    ws.title('Starfield')
-    ws.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{WINDOW_POS}')
+
+def run():
+    root = Tk()
+    root.title('Starfield')
+    root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{WINDOW_POS}')
+    canvas = Canvas(root, bg='black')
+    canvas.pack()
+
+
+    canvas.grid()
+    for s in create_random_starfield():
+        pass
+        canvas.create_oval(s[0], s[1], s[0]+STAR_SIZE, s[1]+STAR_SIZE, fill='white')
+
+
     mainloop()
 
 
@@ -22,4 +41,3 @@ def run():
 if __name__ == '__main__':
     print_hi('PyCharm')
     run()
-
